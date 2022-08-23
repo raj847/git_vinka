93bc8380e7b6a41fd4fb81b5debc550374479d63
raj847 <aryadevaraj19@gmail.com>
2022-08-24 04:06:08 +0700

    task point 4

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"pincok/hitung"
+)
 
 func main() {
 	fmt.Println("Saya vinka Annisa")
+	fmt.Println("fiturA point 4")
+
+	hasilTambah := hitung.Tambah(9, 9)
+	fmt.Println(hasilTambah)
 }
